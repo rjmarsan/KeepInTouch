e3dbc79abf4385edf5549c6ea1fadce946a459ee
Nelson Osacky <nosacky@gmail.com>
2012-11-05 20:56:52 -0600

    simple handling for when user has no favorites

--- src/com/rnm/keepintouch/FavoritesFragment.java
+++ src/com/rnm/keepintouch/FavoritesFragment.java
@@ -12,6 +12,8 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.rnm.keepintouch.data.Contact;
 import com.rnm.keepintouch.data.ContactEvent;
@@ -21,6 +23,10 @@ public class FavoritesFragment extends Fragment implements OnItemClickListener {
 	
 	ContactsAdapter contactsAdapter;
 	List<Contact> contactlist;
+	TextView error;
+	
+	ProgressBar empty;
+	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -30,23 +36,29 @@ public class FavoritesFragment extends Fragment implements OnItemClickListener {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.favorites_list, null);
-		ListView list = (ListView)v.findViewById(R.id.list);
-		list.setDividerHeight(0);
+		error = (TextView)v.findViewById(R.id.no_fav);
+		empty = (ProgressBar)v.findViewById(R.id.empty);
+		ListView listView = (ListView)v.findViewById(R.id.list);
+		listView.setDividerHeight(0);
 		//list.setDivider(getResources().getDrawable(android.R.drawable.menu_frame));
 		contactsAdapter = new ContactsAdapter(getActivity(), R.layout.contact_list_item, ((MainActivity)getActivity()).fav);
-		list.setAdapter(contactsAdapter);
-		list.setOnItemClickListener(this);
-		list.setEmptyView(v.findViewById(R.id.empty));
+		listView.setAdapter(contactsAdapter);
+		listView.setOnItemClickListener(this);
+		listView.setEmptyView(empty);
 		return v;
 	}
 	
 	public void refresh(List<Contact> list) {
-		if (list != contactlist) {
+		if(list.size() == 0){
+			error.setVisibility(View.VISIBLE);
+			empty.setVisibility(View.INVISIBLE);
+		}else if (list != contactlist) {
+			error.setVisibility(View.INVISIBLE);
 			contactlist = list;
 			contactsAdapter.clear();
 			contactsAdapter.addAll(list);
+			contactsAdapter.notifyDataSetChanged();
 		}
-		contactsAdapter.notifyDataSetChanged();
 	}
 
 	@Override
